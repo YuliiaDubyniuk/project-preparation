8593ffa888abbfa09888c2a66baaea57d0afd954
YuliiaDubyniuk <126833026+YuliiaDubyniuk@users.noreply.github.com>
2023-08-31 16:47:17 -0400

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,9 @@
 import axios from 'axios';
 
-const API_KEY = '38922427-a1320c38255791562f8d90b5f';
-const BASE_URL = 'https://pixabay.com/api/';
+// const API_KEY = '38922427-a1320c38255791562f8d90b5f';
+const BASE_URL = 'https://books-backend.p.goit.global/books/top-books';
 
-async function makeRequest(searchTerm, page) {
-  axios.defaults.params = {
-    key: API_KEY,
-    q: searchTerm,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 40,
-    page,
-  };
+async function makeRequestByCategory() {
     
   return await axios.get(BASE_URL);
 } 
--- src/index.js
+++ src/index.js
@@ -19,72 +19,73 @@ const lightbox = new SimpleLightbox('.gallery a', {
 formEl.addEventListener('submit', searchImg);
 loadMoreBtn.addEventListener('click', loadMoreImg);
 
-async function searchImg(evt) {
-  evt.preventDefault();
-  galleryBox.innerHTML = '';
-  totalShownImg = 0;
+// async function searchImg(evt) {
+//   evt.preventDefault();
+//   galleryBox.innerHTML = '';
+//   totalShownImg = 0;
 
-  inputVal = evt.currentTarget.elements.searchQuery.value.trim();
+//   inputVal = evt.currentTarget.elements.searchQuery.value.trim();
 
-  if (inputVal === '') {
-    Notify.info('Please, enter your keyword for search.');
-    return;
-  }
-  createGallery();
-  evt.currentTarget.reset();
-}
+//   if (inputVal === '') {
+//     Notify.info('Please, enter your keyword for search.');
+//     return;
+//   }
+//   createGallery();
+//   evt.currentTarget.reset();
+// }
 
 async function createGallery() {
   try {
-    const { data } = await makeRequest(inputVal, page);
+    const resp = await makeRequest();
+    console.log(resp)
 
-    if (page === 1) {
-      notify(data.total);
-    }
+  //   if (page === 1) {
+  //     notify(data.total);
+  //   }
 
-   addMarkup (data.hits);
+  //  addMarkup (data.hits);
 
-    if (data.total > imgPerPage * page) {
-      loadMoreBtn.classList.remove('visually-hidden');
-    }
-    lightbox.refresh();
+  //   if (data.total > imgPerPage * page) {
+  //     loadMoreBtn.classList.remove('visually-hidden');
+  //   }
+  //   lightbox.refresh();
 
-    totalShownImg += data.hits.length;
+  //   totalShownImg += data.hits.length;
     
-    if (data.total === totalShownImg && data.total !== 0) {
-      Notify.info("We're sorry, but you've reached the end of search results.");
-    };
+  //   if (data.total === totalShownImg && data.total !== 0) {
+  //     Notify.info("We're sorry, but you've reached the end of search results.");
+  //   };
 
-     const { height: cardHeight } = document
-        .querySelector('.gallery')
-        .firstElementChild.getBoundingClientRect();
+  //    const { height: cardHeight } = document
+  //       .querySelector('.gallery')
+  //       .firstElementChild.getBoundingClientRect();
     
-      window.scrollBy({
-        top: cardHeight * 2,
-        behavior: 'smooth',
-      });
+  //     window.scrollBy({
+  //       top: cardHeight * 2,
+  //       behavior: 'smooth',
+  //     });
     
   } catch (error) {
     console.error(error.message);  
   }
 }
 
-function addMarkup(resp) {
-  galleryBox.insertAdjacentHTML('beforeend', createMarkup(resp));
-}
+// function addMarkup(resp) {
+//   galleryBox.insertAdjacentHTML('beforeend', createMarkup(resp));
+// }
 
-function loadMoreImg() {
-  page += 1;
-  loadMoreBtn.classList.add('visually-hidden');
-  createGallery(inputVal);
-}
+// function loadMoreImg() {
+//   page += 1;
+//   loadMoreBtn.classList.add('visually-hidden');
+//   createGallery(inputVal);
+// }
 
-function notify(resp) {
-  if (resp === 0) {
-    Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  } else {
-    Notify.success(`Hooray! We found ${resp} images`);
-  }
-}
+// function notify(resp) {
+//   if (resp === 0) {
+//     Notify.failure(
+//       'Sorry, there are no images matching your search query. Please try again.'
+//     );
+//   } else {
+//     Notify.success(`Hooray! We found ${resp} images`);
+//   }
+// }
